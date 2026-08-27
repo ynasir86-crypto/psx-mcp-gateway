f2578e913a8fdcb0bf88d88916215c2a5ebc6b46
ynasir86-crypto <y.nasir86@gmail.com>
2026-08-28 03:13:12 +0500

    Add OAuth authorization server metadata endpoint
    Implement GET endpoint to return OAuth server metadata.

--- .well-known/oauth-authorization-server/route.ts
+++ .well-known/oauth-authorization-server/route.ts
@@ -1,1 +1,41 @@
+import {
+  OAUTH_ISSUER,
+  OAUTH_CLIENT_ID,
+} from "../../../../lib/oauth";
 
+export const runtime = "nodejs";
+export const dynamic = "force-dynamic";
+
+export async function GET() {
+  return Response.json({
+    issuer: OAUTH_ISSUER,
+
+    authorization_endpoint:
+      `${OAUTH_ISSUER}/authorize`,
+
+    token_endpoint:
+      `${OAUTH_ISSUER}/token`,
+
+    response_types_supported: [
+      "code",
+    ],
+
+    grant_types_supported: [
+      "authorization_code",
+    ],
+
+    code_challenge_methods_supported: [
+      "S256",
+    ],
+
+    token_endpoint_auth_methods_supported: [
+      "none",
+    ],
+
+    scopes_supported: [
+      "mcp",
+    ],
+
+    client_id: OAUTH_CLIENT_ID,
+  });
+}
